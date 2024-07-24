67b6b9788b860f47428ec213ba1d60cf16bcf06f
anitah254-pixel <ann.kilonzo@student.moringaschool.com>
2024-07-24 11:04:22 +0300

    Implement task creation, deletion, and editing functionality in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const inputEl = document.querySelector("input#new-task-description");
+  const createTaskFormEl = document.querySelector("form#create-task-form");
+  const taskListEl = document.querySelector("ul#tasks");
+
+  createTaskFormEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const inputElValue = inputEl.value;
+
+    const listItemEl = document.createElement("li");
+
+    const spanEl = document.createElement("span");
+    spanEl.textContent = inputElValue;
+    spanEl.contentEditable = true;
+    spanEl.title = "Click to edit!";
+
+    listItemEl.appendChild(spanEl);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
+    deleteButton.classList.add("btn", "btn-danger");
+
+    listItemEl.appendChild(deleteButton);
+
+    taskListEl.appendChild(listItemEl);
+
+    inputEl.value = "";
+  });
+
+  taskListEl.addEventListener("click", (event) => {
+    if (event.target.matches("button")) {
+      const listItemEl = event.target.parentElement;
+      listItemEl.remove();
+    }
+  });
+
+  taskListEl.addEventListener("click", (event) => {
+    if (event.target.matches("li")) {
+      event.target.contenteditable = true;
+    }
+  });
 });
